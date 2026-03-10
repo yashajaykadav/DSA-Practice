166ea651046b61faab6c4c8600dd49b46eaf1691
YASHKADAV <110305731+yashajaykadav@users.noreply.github.com>
2026-03-10 22:58:18 +0530

    Added String Duplicate Finder

---
+++ String/DuplicateChars.java
@@ -0,0 +1,32 @@
+package String;
+
+import java.util.*;
+
+public class DuplicateChars {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter string : ");
+        if(!sc.hasNextLine())return;
+
+        String s = sc.nextLine();
+
+        int[] freq = new int[25];
+
+        for(char c : s.toCharArray()){
+            freq[c-'a']++;
+        }
+
+        for(int i = 0 ; i < 25 ;i++){
+            if(freq[i]>1)
+                {
+                    System.out.println((char) (i + 'a') + " - " + freq[i]);
+                }
+        }
+
+        sc.close();
+        
+    }
+    
+}
